30cec67cf21b6d400a0e47fabbee7114cd287b96
phukkoo <priti_hukkoo@yahoo.com>
2015-09-25 12:19:40 -0700

    adding changes to all 3 file

--- README.md
+++ README.md
@@ -3,22 +3,29 @@ GetData_CourseProject
 
 Repositoty contains peer-assessed Coursera's Getting-and-Cleaning-Data Course Project.
 
-David's project FAQ has been used as guidelines for the project. [1]
 
 `run_analysis.R` is a script that performs the following:
 
 1. Downloads data from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
 2. Unzips it into ./data directory
-3. Each of the files for Subject, X and Y, both from test and train data sets is then read into its own data frame using `read.csv`
-4. All data frames are merged using `rbind` to combine test and train datasets and `cbind` to combine Subject, Activity and Measurements.
-5. Column are given meaningful names using hard-coded values for *Subject* and *Activity* and those read programmatically from **features.txt** data set. [2]
-6. Using `grepl` finds all column names containing "mean" or "std". I am excluding columns containing "meanFreq" since functionaly it isn't a measurement of the data from the sensors, but a rate of measurement.
-7. Reads IDs and names of activities from **activity_labels.txt** and uses `factor` to convert activity ids in the dataset to descriptive names of activities. Activity IDs are used as factor levels and activity names are used as labels.
-8. Uses `ddply` to create a second data set with the average of each variable for each activity and each subject. Data is split by variables *Subject* and *Activity*, `colMeans` are used to calculate means on all columns except first two (Subject and Activity).
-9. Uses `write.csv` to save the final tidy dataset as a CSV file names ./data/tidydata.txt. File can be read back with `read.csv("./data/tidydata.txt")`
+3. Each of the files for Subject( Contains Subject num 1 - 30), y ( contains activity info)  and X( contains measurements as a 561 element vector), 
+   both from test and train data sets is then read into its own data frame using `read.table`
+4. All data frames are merged using `rbind` to combine test and train datasets .
+5. Meta data for features and activity are read .
+    * Features names and column info is read from features.txt
+    * Activity info is read in from the activity labels txt
+6. Using `grepl` finds all column names in the measurement data frame containing "mean()" or "std()". I am excluding columns containing "meanFreq" since functionaly it isn't a measurement of the data from the sensors, but a rate of measurement.
+7. Activity IDs are replaced by the respective activity names in the activity data using the factor funtionality.
+8. All data frames are combined using `cbind` to combine Subject, Activity and measurement data.
+9. Column are given meaningful names using hard-coded values for *Subject* and *Activity* and those read programmatically from **features.txt** data set. the feature names are 
+   made compatible with R variable naming conventions like removing special caharacter and replacing '-' by '_'. 
+10. Now we need to create the tidy data set. This tidy data set contain one observation per subject and activity containing the means of the measurements of interest.
+    This was achieved by using 'dplyr' and 'tidyr' 
+    * grouped the above data frame using grouped_by ( subject, activity)
+    * Then in each of the resulting groups find the average of the variables by using summarize.
+    This will result n 180 rows of observation with 68 columns. ( 30 subjects times 6 activities and 66 measurements).
+    Since reading 68 columns my be a bit messy, will tidy this further by gathering(melting) the 66 columns to rows hence Reshaping wide format to long narrow format.
+    The gather() function will take multiple columns and collapse them into key-value pairs, duplicating all other columns as needed.
+    * then rearranging coumns so we see Subject, Measurement , Activity and Value.
+11. Uses `write.table` to save the final tidy dataset as a CSV file names ./data/tidydata.txt. File can be read back with `read.table("./data/tidydata.txt")`
 
-#### References:
-
-[1] https://class.coursera.org/getdata-007/forum/thread?thread_id=49
-
-[2] https://class.coursera.org/getdata-007/forum/thread?thread_id=99#post-442
--- run_analysis.R
+++ run_analysis.R
@@ -1,14 +1,26 @@
 library("dplyr")
 library("tidyr")
+## Check if the data directory exists and create if it doesn't
+if(!file.exists("./getdata")) {
+        dir.create("./getdata")
+}
+
+## Download and unzip dataset
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+tempFile <- tempfile()
+download.file(fileURL, tempFile, method = "curl")
+unzip(tempFile, exdir = "./getdata")
+file.remove(tempFile)
+
 # read test data
-subject_data_test <- read.table("/Users/phukkoo/coursera_getdata_class/UCI HAR Dataset/test/subject_test.txt")
-activity_data_test <- read.table("/Users/phukkoo/coursera_getdata_class/UCI HAR Dataset/test/y_test.txt")
-readings_data_test <- read.table("/Users/phukkoo/coursera_getdata_class/UCI HAR Dataset/test/X_test.txt")
+subject_data_test <- read.table("./getdata/UCI HAR Dataset/test/subject_test.txt")
+activity_data_test <- read.table("./getdata/UCI HAR Dataset/test/y_test.txt")
+readings_data_test <- read.table("./getdata/UCI HAR Dataset/test/X_test.txt")
 
 # read train data
-subject_data_train <- read.table("/Users/phukkoo/coursera_getdata_class/UCI HAR Dataset/train/subject_train.txt")
-activity_data_train <- read.table("/Users/phukkoo/coursera_getdata_class/UCI HAR Dataset/train/y_train.txt")
-readings_data_train <- read.table("/Users/phukkoo/coursera_getdata_class/UCI HAR Dataset/train/X_train.txt")
+subject_data_train <- read.table("./getdata/UCI HAR Dataset/train/subject_train.txt")
+activity_data_train <- read.table("./getdata/UCI HAR Dataset/train/y_train.txt")
+readings_data_train <- read.table("./getdata/UCI HAR Dataset/train/X_train.txt")
 
 # combine the  test and train data as the Requirement 1 of the project
 subject_data <- rbind(subject_data_test, subject_data_train)
@@ -16,14 +28,16 @@ activity_data <- rbind(activity_data_test, activity_data_train)
 readings_data <- rbind(readings_data_test, readings_data_train)
 
 # read features 
-features_data <- read.table("/Users/phukkoo/coursera_getdata_class/UCI HAR Dataset/features.txt")
+features_data <- read.table("./getdata/UCI HAR Dataset/features.txt")
 # read activity info
-activity_labels <- read.table("/Users/phukkoo/coursera_getdata_class/UCI HAR Dataset/activity_labels.txt")
+activity_labels <- read.table("./getdata/UCI HAR Dataset/activity_labels.txt")
 
 #get only rows containing mean and std 
 # using regular expressions for this
 reqd_features  <- filter(features_data, grepl(".*-mean\\(\\)-*|*-std\\(\\)-|*-mean\\(\\)$|*-std\\(\\)$",V2))
-# this will give the column indices 
+# reqd_features column 1(v1) gives the column indices of the columns we need
+# reqd_features column 2(v2) gives the column names of the columns we need
+# this will give the column indices of the features that contain mean and std deviation
 reqd_features_cols  <- reqd_features$V1
 
 # Here we read only the columns we are interested in from the main data frame 
@@ -36,29 +50,45 @@ activity_name_data <- factor(activity_data$V1, levels = activity_labels[, 1], la
 
 # use cbind to combine the subject, activity and measurement reading data.
 # to get the data set which we can use to summarize and tidy
-#complete_data <- cbind(cbind(subject_data,complete_activity_data),reqd_data)
 complete_data <- cbind(subject_data, activity_name_data,reqd_data)
 
 
 #Appropriately labels the data set with descriptive variable names. 
+# first we will tidy up the column names to be compliant with variable naming conventions of R
 #fulfilling  Requirement 4 of  the project
-colnames(complete_data) <- c("Subject","Activity", as.vector(reqd_features$V2))
+
+clean_req_feature_names = gsub("\\(\\)", "", reqd_features$V2)
+clean_req_feature_names = gsub("-", "_", clean_req_feature_names)
+colnames(complete_data) <- c("Subject","Activity", as.vector(clean_req_feature_names))
 
 
 # now group the data by Subject and Activity Name and get the mean values for 
 # From the data set in step 4, creates a second, independent tidy data set
 # with the average of each variable for each activity and each subject
+
+# first get the dim of the data set do we know how many columns are present
 dim_of_data <- dim(complete_data)
+
+# using dplyr function
+# group_by to group the data by subject and activity 
+# and then in each of the resulting groups find the average of the variables by using summarize.
+# this will give us tidy_data is wide format of 180 rows and 68 columnns ( 2 for subject and activity and the other 66 
+# for the variables we need)
 wide_tidy_data <- complete_data  %>% 
         group_by(Subject, Activity)  %>%
         summarise_each(funs(mean)) 
-       
+
+# since  reading 68 columns my be a bit messy, will tidy this further by gathering(melting) the 66 columns to rows instead
+# using tidyr function gather and  Reshape wide format to long narrow format
+# then rearranging columns so we see 
+# Subject, Measure , Activity and value of Measure
 narrow_tidy_data <- wide_tidy_data %>% 
         gather( Measure, Average, 3:dim_of_data[2] ) %>%
         select(Subject, Measure, Activity, Average)  %>%
         arrange(Subject, Measure)
 
-write.table(wide_tidy_data, "/Users/phukkoo/coursera_getdata_class/UCI HAR Dataset/wide_tidy_data.txt", row.name=FALSE)
-write.table(narrow_tidy_data, "/Users/phukkoo/coursera_getdata_class/UCI HAR Dataset/narrow_tidy_data.txt", row.name=FALSE)
+# here we write out the tidy_data into a txt file.
+write.table(narrow_tidy_data, "./getdata/UCI HAR Dataset/tidy_data.txt", row.name=FALSE)
 
-df2 <- read.table("/Users/phukkoo/coursera_getdata_class/UCI HAR Dataset/tidy_data1.txt")
+# this is just to read back the tidy data from txt and check the output to verify that the file was created correctly
+tidy_df <- read.table("./getdata/UCI HAR Dataset/tidy_data.txt", header = TRUE)
